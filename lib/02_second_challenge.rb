babbeeb08e967521dcc41f76ab9e07ca24ffe962
Sara Bastian <seb118@georgetown.edu>
2020-09-17 13:52:52 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -4,8 +4,8 @@ def second_challenge
       :patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+      :patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
+    }
 
   
 end
